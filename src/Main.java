b2d47dbccefffb09c726176b26b629a87674192e
tatsunori <tatsunoripai@gmail.com>
2017-11-17 21:01:21 -0200

    sei laasdsa

--- src/Main.java
+++ src/Main.java
@@ -70,6 +70,7 @@ class Main {
 			}
 		}
 	}
+
 	public static int[] findZero(int[][] matriz){
 		int i,j;
 		int ij[] = new int[2];
@@ -84,6 +85,7 @@ class Main {
 		}
 		return ij;
 	}
+
 	public static int[][] copyMatriz(int [][] original){
 		int i,j;
 		int [][]copia = new int[4][4];
@@ -94,43 +96,31 @@ class Main {
 		}
 		return copia;
 	}
-	public static int[][] changeUp(No no, int i, int j){
-		int aux;
-		int matriz[][] = copyMatriz(no.state);
-		aux = matriz[i][j];
-		matriz[i][j] = matriz[i-1][j];
-		matriz[i-1][j] = aux;
 
+	public static int[][] changeUp(No no, int i, int j){
+		int matriz[][] = copyMatriz(no.state);
+		matriz[i][j] = matriz[i-1][j];
+		matriz[i-1][j] = 0;
 		return matriz;
 	}
 	public static int[][] changeDown(No no, int i, int j){
-		int aux;
 		int matriz[][] = copyMatriz(no.state);
-		aux = matriz[i][j];
 		matriz[i][j] = matriz[i+1][j];
-		matriz[i+1][j] = aux;
-
+		matriz[i+1][j] = 0;
 		return matriz;
 	}
 	public static int[][] changeRight(No no, int i, int j){
-		int aux;
 		int matriz[][] = copyMatriz(no.state);
-		aux = matriz[i][j];
 		matriz[i][j] = matriz[i][j+1];
-		matriz[i][j+1] = aux;
-
+		matriz[i][j+1] = 0;
 		return matriz;
 	}
 	public static int[][] changeLeft(No no, int i, int j) {
-		int aux;
 		int matriz[][] = copyMatriz(no.state);
-		aux = matriz[i][j];
 		matriz[i][j] = matriz[i][j-1];
-		matriz[i][j-1] = aux;
-
+		matriz[i][j-1] = 0;
 		return matriz;
 	}
-
 }
 
 class No implements Comparable<No>{
@@ -158,11 +148,72 @@ class No implements Comparable<No>{
 		this.j = j;
         this.state = h;
 		this.g = g;
-		this.h = ficarEmOrdem(h);
+		this.h = euristica1(h);
 		this.f = this.g + this.h;
 	}
 
-	public int ficarEmOrdem(int matriz[][]){
+
+	public int euristica1(int matriz[][]){
+    	int resp = 0;
+		int i,j;
+		int teste[][] = {{1, 2, 3, 4}, {12, 13, 14, 5}, {11, 0, 15, 6}, {10, 9, 8, 7}};
+
+
+		for(i=0; i<4; i++){
+			for(j=0; j<4; j++){
+				if(matriz[i][j] == teste[i][j] || matriz[i][j] == 0){
+
+				}else{
+					resp++;
+				}
+			}
+		}
+
+    	return resp;
+	}
+
+	public int euristica2(int matriz[][]){
+		int resp;
+		int vetor[] = new int[16];
+		vetor[0] = matriz[0][0];
+		vetor[1] = matriz[0][1];
+		vetor[2] = matriz[0][2];
+		vetor[3] = matriz[0][3];
+
+		vetor[4] = matriz[1][3];
+		vetor[5] = matriz[2][3];
+		vetor[6] = matriz[3][3];
+
+		vetor[7] = matriz[3][2];
+		vetor[8] = matriz[3][1];
+		vetor[9] = matriz[3][0];
+
+		vetor[10] = matriz[2][0];
+		vetor[11] = matriz[1][0];
+
+		vetor[12] = matriz[1][1];
+		vetor[13] = matriz[1][2];
+
+		vetor[14] = matriz[2][2];
+		vetor[15] = matriz[2][1];
+
+		return resp = (euristica2part2(vetor));
+
+	}
+
+	public int euristica2part2(int vetor[]){
+		int resp = 0;
+		for(i=0;i<15;i++) {
+			if((vetor[i] == (vetor[i+1]-1)) || vetor[i] == 0){
+
+			}else{
+				resp++;
+			}
+		}
+		return resp;
+	}
+
+	public int euristica3(int matriz[][]){
 		int i1,j1,aux;
 		int resp = 0;
 
@@ -170,7 +221,7 @@ class No implements Comparable<No>{
 			for(j1 = 0; j1 < 4; j1++){
 
 				aux = matriz[i1][j1];
-				resp = resp + ijCerto(i1, j1, aux);
+				resp = resp + eurista3part2(i1, j1, aux);
 			}
 		}
 		return resp;
@@ -178,7 +229,7 @@ class No implements Comparable<No>{
 
 	}
 
-	public int ijCerto(int i1, int j1, int num) {
+	public int eurista3part2(int i1, int j1, int num) {
 		int i2, j2;
 		int teste[] = {0, 0, 1, 2, 3, 7, 11, 15, 14, 13, 12, 8, 4, 5, 6, 10};
 
@@ -191,6 +242,11 @@ class No implements Comparable<No>{
 
 		return (Math.abs(i1 - i2) + Math.abs(j1 - j2));
 
+	}
+
+	public int euristica5(int matriz[][]){
+		int aux = Math.max(euristica1(matriz), euristica2(matriz));
+		return Math.max(aux, euristica3(matriz));
 	}
 
     @Override
